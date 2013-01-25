4307f173cda77bffc19f2c677d52c77f672f0fe6
Andrew Couch <acouch@bluewolfgroup.com>
2013-01-25 18:54:55 -0500

    basic constructor

--- test/force_test.js
+++ test/force_test.js
@@ -20,15 +20,28 @@ var force_js = require('../lib/force.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+exports['Connection'] = {
   setUp: function(done) {
     // setup here
     done();
   },
   'no args': function(test) {
     test.expect(1);
-    // tests here
-    test.equal(force_js.awesome(), 'awesome', 'should be awesome.');
+    var conn = new force_js.Connection();
+    test.ok(conn, 'should be empty');
+    test.done();
+  },
+  'four args': function(test) {
+    test.expect(4);
+    var login = {};
+    var resource = {};
+    var client_id = {};
+    var client_secret = {};
+    var conn = new force_js.Connection(login, resource, client_id, client_secret);
+    test.strictEqual(conn.login_url, login, 'the login url should be loaded');
+    test.strictEqual(conn.resource_url, resource, 'the resource url should be loaded');
+    test.strictEqual(conn.client_id, client_id, 'the client id should be loaded');
+    test.strictEqual(conn.client_secret, client_secret, 'the client secret should be loaded');
     test.done();
   }
 };
