c3a09c4ea98b4d7afd2f9a4ec14fe43825ab9b86
Andrew Couch <andy@couchand.com>
2013-02-08 10:26:29 -0500

    integration test: indent

--- test/integration_test.js
+++ test/integration_test.js
@@ -10,76 +10,76 @@ module.exports['Integration'] = {
     });
   },
   'REST API' : {
-  'list resources, objects, etc.': function(test) {
-    test.expect(11);
-    var conn = this.conn;
+    'list resources, objects, etc.': function(test) {
+      test.expect(11);
+      var conn = this.conn;
 
-    conn.listVersions().then(function(versions) {
-      test.ok(versions instanceof Array);
+      conn.listVersions().then(function(versions) {
+        test.ok(versions instanceof Array);
 
-      return conn.listRecent();
-    }).then(function(recent) {
-      test.ok(recent instanceof Array);
+        return conn.listRecent();
+      }).then(function(recent) {
+        test.ok(recent instanceof Array);
 
-      return conn.listResources();
-    }).then(function(resources) {
-      test.ok(resources.hasOwnProperty('sobjects'));
-      test.ok(resources.hasOwnProperty('search'));
-      test.ok(resources.hasOwnProperty('query'));
-      test.ok(resources.hasOwnProperty('chatter'));
-      test.ok(resources.hasOwnProperty('connect'));
-      test.ok(resources.hasOwnProperty('tooling'));
+        return conn.listResources();
+      }).then(function(resources) {
+        test.ok(resources.hasOwnProperty('sobjects'));
+        test.ok(resources.hasOwnProperty('search'));
+        test.ok(resources.hasOwnProperty('query'));
+        test.ok(resources.hasOwnProperty('chatter'));
+        test.ok(resources.hasOwnProperty('connect'));
+        test.ok(resources.hasOwnProperty('tooling'));
 
-      return conn.listSObjects();
-    }).then(function(sobjects) {
-      test.ok(sobjects instanceof Array);
-      test.ok(sobjects[0].hasOwnProperty('name'));
+        return conn.listSObjects();
+      }).then(function(sobjects) {
+        test.ok(sobjects instanceof Array);
+        test.ok(sobjects[0].hasOwnProperty('name'));
 
-      return conn.describe(sobjects[0].name);
-    }).then(function(describe) {
-      test.ok(describe.hasOwnProperty('name'));
+        return conn.describe(sobjects[0].name);
+      }).then(function(describe) {
+        test.ok(describe.hasOwnProperty('name'));
 
-      test.done();
-    });
-  },
-  'make and find sobjects': function (test) {
-    test.expect(11);
-    var conn = this.conn;
-    var contact_data = {
-      'FirstName': 'Bruce',
-      'LastName': 'Wayne'
-    };
-    var account_data = {
-      'Name': 'Wayne Industries'
-    };
-    conn.insert('Account', account_data).then(function(account_id) {
-      test.ok(account_id);
-      account_data.Id = account_id;
-      contact_data.AccountId = account_id;
-      return conn.insert('Contact', contact_data);
-    }).then(function(contact_id) {
-      test.ok(contact_id);
-      contact_data.Id = contact_id;
-      return conn.query('select Id, Name from Account where Id = \'' + account_data.Id + "'");
-    }).then(function(accounts) {
-      test.ok(accounts);
-      test.equal(accounts.length, 1);
-      return accounts[0];
-    }).then(function(new_account) {
-      test.equal(new_account.Name, account_data.Name);
-      test.equal(new_account.Id, account_data.Id);
-    }).then(function() {
-      return conn.query('select Id, AccountId, FirstName, LastName from Contact where Id = \'' + contact_data.Id + "'");
-    }).then(function(contacts) {
-      test.equal(contacts.length, 1);
-      return contacts[0];
-    }).then(function(new_contact) {
-      test.equal(new_contact.FirstName, 'Bruce');
-      test.equal(new_contact.LastName, 'Wayne');
-      test.equal(new_contact.AccountId, account_data.Id);
-      test.equal(new_contact.Id, contact_data.Id);
-      test.done();
-    });
-  }
+        test.done();
+      });
+    },
+    'make and find sobjects': function (test) {
+      test.expect(11);
+      var conn = this.conn;
+      var contact_data = {
+        'FirstName': 'Bruce',
+        'LastName': 'Wayne'
+      };
+      var account_data = {
+        'Name': 'Wayne Industries'
+      };
+      conn.insert('Account', account_data).then(function(account_id) {
+        test.ok(account_id);
+        account_data.Id = account_id;
+        contact_data.AccountId = account_id;
+        return conn.insert('Contact', contact_data);
+      }).then(function(contact_id) {
+        test.ok(contact_id);
+        contact_data.Id = contact_id;
+        return conn.query('select Id, Name from Account where Id = \'' + account_data.Id + "'");
+      }).then(function(accounts) {
+        test.ok(accounts);
+        test.equal(accounts.length, 1);
+        return accounts[0];
+      }).then(function(new_account) {
+        test.equal(new_account.Name, account_data.Name);
+        test.equal(new_account.Id, account_data.Id);
+      }).then(function() {
+        return conn.query('select Id, AccountId, FirstName, LastName from Contact where Id = \'' + contact_data.Id + "'");
+      }).then(function(contacts) {
+        test.equal(contacts.length, 1);
+        return contacts[0];
+      }).then(function(new_contact) {
+        test.equal(new_contact.FirstName, 'Bruce');
+        test.equal(new_contact.LastName, 'Wayne');
+        test.equal(new_contact.AccountId, account_data.Id);
+        test.equal(new_contact.Id, contact_data.Id);
+        test.done();
+      });
+    }
   }
 };
