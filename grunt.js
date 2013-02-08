4c92d7d8edbbd257b426a46f71863b5842df03c3
Andrew Couch <andy@couchand.com>
2013-02-08 16:40:42 -0500

    gruntfile: dist tasks

--- grunt.js
+++ grunt.js
@@ -5,12 +5,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
+    meta: {
+      banner: '/*\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+              ' * built <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+              '<%= pkg.homepage ? " * " + pkg.homepage + "\n" : "" %>' +
+              ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+              ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */'
+    },
     test: {
       unit: ['test/unit/*.js'],
       integration: ['test/integration/*.js']
     },
     lint: {
       files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/force.js>'],
+        dest: 'dist/force-<%= pkg.version %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/force-<%= pkg.version %>.min.js'
+      }
     },
     watch: {
       files: '<config:lint.files>',
@@ -36,7 +55,9 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('dist','concat min');
+
   // Default task.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', 'lint test dist');
 
 };
