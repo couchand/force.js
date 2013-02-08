19ffba0f907516af810a8f77476c2990fb9365ba
Andrew Couch <andy@couchand.com>
2013-02-07 20:48:01 -0500

    add notes to the example file

--- auth.js
+++ auth.js
@@ -8,3 +8,27 @@ var c = new sfdc.Connection('prerellogin.pre.salesforce.com','prerelna1.pre.sale
 c.authorize('couchand+spring13@gmail.com','','La8Mu2IqdEuQpqrjggHuIvd9v').then(function(){console.log('in');});
 
 
+var mystuff = {}
+c.tooling.insert('metadatacontainer',{'name':'fod'}).then(function(id){mystuff.container = id;});
+
+c.tooling.query('select id, body from apexclass where name = \'foobar\'').then(function(res){mystuff.body = res[0].Body;mystuff.cls = res[0].Id;});
+
+mystuff.body2 = mystuff.body.substr(0,mystuff.body.length-1)+"\n\tpublic void doNothing()\n\t{\n\t}\n}"
+
+c.tooling.insert('apexclassmember',{'metadatacontainerid':mystuff.container,'contententityid':mystuff.cls,'body':mystuff.body2}).then(function(id){mystuff.acm = id;console.log(id);});
+
+c.tooling.getSObject('apexclassmember',mystuff.acm).then(console.log)
+
+c.tooling.deploy(mystuff.container,true).then(console.log)
+
+c.tooling.getSObject('apexclassmember',mystuff.acm).then(console.log)
+
+c.tooling.getSObject('apexclassmember',mystuff.acm).then(function(res){mystuff.st = res.SymbolTable;});
+
+
+//now try a failed deployment!
+// watch node.js crash!
+//then try again!
+
+c.tooling.deploy('1dcx000000000BKAAY',true).then(console.log,function(err){console.log('err!',err.ErrorMsg,err.CompilerErrors);});
+
